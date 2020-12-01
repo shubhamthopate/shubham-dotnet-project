efa1f4fa1c050c7d391f08dfd9133925eab4ff40
shubham <shubhamthopate9960@gmail.com>
2020-12-01 14:14:27 +0530

    first commit

---
+++ BookListRazor/wwwroot/js/bookList.js
@@ -0,0 +1,72 @@
+﻿
+var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT-load').DataTable({
+        "ajax": {
+            "url": "/api/book",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "20%" },
+            { "data": "auther", "width": "20%" },
+            { "data": "isbn", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/BookList/Upsert?id=${data}" class="btn btn-success text-white" style="cursor:pointer;width:70px">
+                            Edit
+                         </a>
+                           &nbsp;
+                           <a class="btn btn-danger text-white" style="cursor:pointer;width:70px"
+                            onclick=Delete('/api/book?id='+${data})>
+                            Delete
+                         </a>
+                </div>`;
+                },
+                "width": "40%"
+
+            }
+        ],
+        "language": {
+            "emptyTable": "no Data found"
+        },
+        "width": "100%"
+
+    })
+}
+function Delete(urls) {
+    console.log(urls);
+    swal({
+        title: "Are you Sure?",
+        text: "Once Deleted , cannot be able to recover",
+        icon: "warning",
+        button:true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: urls,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+    console.log(urls);
+}
+
+
+
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: dotnet exec ./BookListRazor.dll --urls http://0.0.0.0:5000/
